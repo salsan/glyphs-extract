94a58ac768f8d81da223a3ec0380f7caa2b0d531
Salvatore Santagati <github@salsan.dev>
2022-06-12 10:07:21 +0200

    chore: added new assets for test

--- test/svg.spec.ts
+++ test/svg.spec.ts
@@ -3,11 +3,11 @@ const fs = require('fs')
 const svg = require('../src/svg')
 const convert = require('../src/index')
 
-const data = fs.readFileSync('./test/assets/Chess.svg', 'utf8')
+const data = fs.readFileSync('./test/assets/fonts/Chess.svg', 'utf8')
 
 describe('Function init()', function () {
   it('Pass data from File', function () {
-    const result = svg.init(data, { format: 'svg' })
+    const result = svg.init(data)
     assert.ok(typeof (result) === 'object', 'The value is not an object')
   })
 })
